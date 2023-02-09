e14d0357f67950527107e2531d3135f9321602ba
happy <happyutiheba@gmail.com>
2023-02-09 21:47:11 +0200

    modified

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+ const router = require("express").Router();
 //const Joi = require('joi');
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
